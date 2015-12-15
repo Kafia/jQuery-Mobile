90dce3f63b0da1a070334c6a9b1aa45069b0c787
Kafia Ahmed <kafia@sutlej.net>
2015-12-15 21:31:46 +0500

    jquery mobile events

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-$(document).on("pagecreate","#process",function(){
+/*jQuery Mobile PageInit click Event */
+$(document).on("pageinit","#process",function(){
   $("p").on("click",function(){
     $(this).hide();
+  });                       
+});
+/*jQuery Mobile Tap Event */
+$(document).on("pageinit","#home",function(){
+  $("p").on("tap",function(){
+    $(this).hide();
+  });                       
+});
+/*jQuery Mobile Vmouseover Event */
+$( document ).on( "vmouseover", "span", function() {
+  $( this ).append( "<span style='color:#108040;'> Dgenius...</span>" );
+});
+
+/*jQuery Mobile Swipe Event */
+$(document).on("pageinit","#process",function(){
+  $("p").on("swipe",function(){
+    $("span").text("Swipe detected!");
   });                       
 });
